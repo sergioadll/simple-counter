ac80e0946978f63b826d160381f532d2e5d24b1e
sergioadll <sergio.adll@hotmail.com>
2020-10-30 04:07:50 +0000

    added functionalities, missing stop and resume

--- src/js/component/counter.js
+++ src/js/component/counter.js
@@ -4,19 +4,26 @@ import { BsClockHistory } from "react-icons/bs";
 
 //create your first component
 export function Counter(props) {
-	const { value } = props;
+	const { value, left } = props;
+	let time = value;
+	if (left <= 0 && left > -10 && value[0] == 0) {
+		alert("Time is up");
+	} else if (left < 0) {
+		time = [0, 0, 0, 0];
+	}
 	return (
 		<div className="outter-box d-flex">
 			<div className="card clock">
 				<BsClockHistory size={100} />
 			</div>
-			<div className="card housands">{value[3]}</div>
-			<div className="card hundreds">{value[2]}</div>
-			<div className="card tens">{value[1]}</div>
-			<div className="card unit">{value[0]}</div>
+			<div className="card housands">{time[3]}</div>
+			<div className="card hundreds">{time[2]}</div>
+			<div className="card tens">{time[1]}</div>
+			<div className="card unit">{time[0]}</div>
 		</div>
 	);
 }
 Counter.propTypes = {
-	value: PropTypes.array
+	value: PropTypes.array,
+	left: PropTypes.number
 };
--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,106 @@
-//import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
-
-//include bootstrap npm library into the bundle
-import "bootstrap";
-
-//include your index.scss file into the bundle
-import "../styles/index.scss";
-
-//import your own components
 import { Counter } from "./component/counter.js";
+import "bootstrap";
+import "../styles/index.scss";
+//import { Counter } from "./component/counter.js";
 
-//render your react application
+let forward = true;
+let seconds = 200;
 let counter = 0;
-setInterval(function() {
+let warningTime = null;
+
+function warning(warningTime) {
+	setTimeout(function() {
+		alert("Hello");
+	}, warningTime);
+}
+
+let reloj = setInterval(function() {
+	let timer = [];
+	timer[0] = Math.floor(seconds) % 10;
+	timer[1] = Math.floor(seconds / 10) % 10;
+	timer[2] = Math.floor(seconds / 100) % 10;
+	timer[3] = Math.floor(seconds / 1000) % 10;
+	seconds--;
 	let value = [];
 	value[0] = Math.floor(counter) % 10;
 	value[1] = Math.floor(counter / 10) % 10;
 	value[2] = Math.floor(counter / 100) % 10;
 	value[3] = Math.floor(counter / 1000) % 10;
 	counter++;
-	ReactDOM.render(<Counter value={value} />, document.querySelector("#app"));
+
+	ReactDOM.render(
+		<div>
+			<div
+				className="pointer button bg-light"
+				onClick={() => {
+					//alert("OK");
+					counter = 0;
+					seconds = warningTime;
+				}}>
+				<b>Reset Time</b>
+			</div>
+			<div
+				className="pointer button bg-primary"
+				onClick={() => {
+					forward = !forward;
+					counter = 0;
+					seconds = 200;
+				}}>
+				{forward && (
+					<span>
+						<b>Timer</b>
+						/Timekeeper
+					</span>
+				)}
+				{!forward && (
+					<span>
+						Timer/
+						<b>Timekeeper</b>
+					</span>
+				)}
+			</div>
+			{forward && <Counter value={value} />}
+			{!forward && (
+				<form className="pointer button bg-warning">
+					<label className="pr-1" htmlFor="fname">
+						<b>Warning at:</b>
+					</label>
+					<input
+						type="text"
+						id="fname"
+						name="fname"
+						onChange={e => {
+							seconds = e.target.value;
+							warningTime = e.target.value;
+						}}
+					/>
+				</form>
+			)}
+			{!forward && <Counter value={timer} left={seconds} />}
+		</div>,
+
+		document.querySelector("#app")
+	);
 }, 1000);
+
+/* STOP AND RESUME FUCTIONS MISSING RESUME TO WORK
+			<div
+				className="pointer button bg-light"
+				onClick={() => {
+					//alert("OK");
+					clearInterval(reloj);
+				}}>
+				Stop Time
+			</div>
+
+			<div
+				className="pointer button bg-dark"
+				onClick={() => {
+					//alert("OK");
+					setInterval(reloj);
+				}}>
+				Resume Time
+			</div>
+*/
